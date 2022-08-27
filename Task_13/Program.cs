05787444e8b6ef22a7e8931f76b7c33ae4e059c6
vovanone2005 <voevoda.junior@mail.ru>
2022-08-27 08:54:41 +0300

    Task_15

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+Console.Write("a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 6)
+{
+  Console.WriteLine("Будний день");
+}  
+else 
+{
+    Console.WriteLine("Выходной, ура ура");
+} 
