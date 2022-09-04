aabcc2819440da7562ffcaf2a7a22c68300df8a7
vovanone2005 <voevoda.junior@mail.ru>
2022-09-04 16:06:02 +0300

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число A");
+Console.Write("A:  ");
+double A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень B");
+Console.Write("B:  ");
+double  B = Convert.ToInt32(Console.ReadLine());
+double  exponent = Math.Pow(A,B);
+Console.WriteLine(+exponent );
